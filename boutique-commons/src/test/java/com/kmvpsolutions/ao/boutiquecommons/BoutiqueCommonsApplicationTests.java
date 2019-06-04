d42083fbe3c823add8d9afea58e4075264721f7e
Kalil Peixoto <kalil.peixoto@jupiter.co.ao>
2019-06-04 19:11:46 +0100

    - Boutique commons, eureka server and customer-service modules

---
+++ boutique-commons/src/test/java/com/kmvpsolutions/ao/boutiquecommons/BoutiqueCommonsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kmvpsolutions.ao.boutiquecommons;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BoutiqueCommonsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
